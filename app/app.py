2c4010cf848408bfe3e80428f7888d9a2cb76e40
Kakularam <kumar.kakularam@gmail.com>
2025-05-30 09:53:51 +0200

    updated code

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ line_count = st.slider('Select a line count', 1, 10, 3)
 
 # and used to select the displayed lines
 head_df = df.head(line_count)
-
+head_df
 number = st.number_input('Insert a number')
 
 st.write('The current number is ', number)
